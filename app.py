26943743bda1ef3d036f94dd1581c64505aa75f8
tappa <167602853+tappa6@users.noreply.github.com>
2025-02-17 08:49:04 +0000

    変更なし

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/hello")
+def hello():
+    return "<p>hello2</p>"
+
+@app.route("/count/<name>")
+def profile(name):
+    return str(len(name))
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/sum", methods=['POST'])
+def _sum():
+    x = int(request.form['x'])
+    y = int(request.form['y'])
+    result = x + y
+    return str(result)
